d15451d4a2a1f7319b8c5e6bc2f4390ac26c5b96
Shakshor <shakshor584@gmail.com>
2022-05-04 15:51:37 +0600

    added steps

---
+++ _steps.js
@@ -0,0 +1,34 @@
+/**
+ * ------------------
+ * Basic Server Setup
+ * -------------------
+ * 1. create a folder (manually or mkdir)
+ * 2. open command line to that folder.
+ * 3. npm init (entry point: index.js)
+ * 4. npm install express
+ * 5. create index.js
+ * ----------------
+ * For Express/ Node Server
+ * ----------------
+ * 1. require express 
+ * 2. create app variable
+ * 3. declare port
+ * 4. set app.get('/')
+ * 5. listen to port 
+ * 6. node index.js
+ * 7. check your browser for that port
+ * ------------------------ 
+ * ---------
+ * get
+ * ---------
+ * 1. cors middleware
+ * 2. npm install cors
+ * 3. require cors
+ * 4. app. use(cors())
+
+ * ------------------
+ * post
+ * -----------------
+ * 1. express.json middleware
+ * 2. app.use(express.json())
+*/
